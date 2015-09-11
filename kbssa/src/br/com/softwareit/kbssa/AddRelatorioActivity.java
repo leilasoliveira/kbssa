f8e9a555132d77034089a3e5fc74408cbdc61613
Leila Oliveira <leila.s.oliveira@hotmail.com>
2015-09-11 09:27:19 -0300

    tipo intensidade da dor do layout alterado

--- kbssa/src/br/com/softwareit/kbssa/AddRelatorioActivity.java
+++ kbssa/src/br/com/softwareit/kbssa/AddRelatorioActivity.java
@@ -1,5 +1,7 @@
 package br.com.softwareit.kbssa;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Date;
 
 import android.app.Activity;
@@ -8,6 +10,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Toast;
 
 public class AddRelatorioActivity extends Activity {
@@ -23,7 +26,7 @@ public class AddRelatorioActivity extends Activity {
 		dao.open();
 		
 		final EditText usernameText = (EditText) findViewById(R.id.user_name_text);
-		final EditText intensidadeText = (EditText) findViewById(R.id.intensidade_text);
+		final RatingBar intensidadeText = (RatingBar) findViewById(R.id.intensidade_text);
 		final EditText comentarioText = (EditText) findViewById(R.id.comentario_text);
 
 		Button saveButton = (Button) findViewById(R.id.add_relatorio_button);
@@ -34,7 +37,7 @@ public class AddRelatorioActivity extends Activity {
 			public void onClick(View v) {
 				String username = usernameText.getEditableText().toString();
 				String comentario = comentarioText.getEditableText().toString();
-				Integer intensidade = Integer.parseInt(intensidadeText.getEditableText().toString());
+				Integer intensidade = new BigDecimal(intensidadeText.getRating()).setScale(0, RoundingMode.UP).intValue();
 				Date data = new Date();
 				
 				dao.create(comentario, intensidade, data, username);
